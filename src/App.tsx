46ec0299ce76119f22241dd31c33cf3819c38b22
“Dharmin <Dharminlive1213@gmail.com>
2021-04-28 21:20:11 -0500

    Feat:Cart Items
    Functionality to add items to cart successful.

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,22 @@ const App = () => {
 
   const getTotalItems = (items:CartItemType[]) =>
   items.reduce((ack:number,item)=> ack + item.amount, 0);
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev=> {
+      // 1. Is the item already in the cart?
+      const isItemInCart = prev.find(item=>item.id === clickedItem.id)
+
+      if(isItemInCart) {
+        return prev.map(item=> (
+          item.id === clickedItem.id
+          ? {...item,amount:item.amount + 1}
+          : item
+        ))
+      }
+      // first time the item is added 
+      return [...prev,{...clickedItem,amount: 1}]
+    })
+  };
   const HandleRemovefromCart = () => null;
 
 
@@ -56,7 +71,7 @@ const App = () => {
       </Drawer>
 <StyledButton onClick={()=>setCartOpen(true)}>
 <Badge badgeContent={getTotalItems(cartItems)} color='error'>
-<AddShoppingCartIcon /> 
+<AddShoppingCartIcon />
 </Badge>
 </StyledButton>
       <Grid container spacing={3}>
